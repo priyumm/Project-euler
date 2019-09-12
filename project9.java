dd0bb0f0c64f1a445d592aaf1aa2f9e404ca7073
priyumm <53181943+priyumm@users.noreply.github.com>
2019-09-12 08:37:44 +0530

    Update project9.java

--- project9.java
+++ project9.java
@@ -1,3 +1,6 @@
+//Problem 9
+/*There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc*/
 public class project9 {
 
 	public static void main(String[] args) {
@@ -13,3 +16,4 @@ public class project9 {
 			}
 	}
 }
+//Output: 31875000
