93dcf03f0526c576e482c8d900e1c0e7ed851509
priyumm <53181943+priyumm@users.noreply.github.com>
2019-09-14 19:59:10 +0530

    Update project6.java

--- project6.java
+++ project6.java
@@ -1,27 +1,13 @@
 //Problem 6
 /*Find the difference between the sum of the squares of the first one hundred 
 natural numbers and the square of the sum..*/
-public class project6 {
+public class project6 { 
+	public static void main(String[] args) { 
+		int size = 100;
+		long qos = (long) Math.pow(size * (size + 1) / 2, 2);
+		long soq = size * (size + 1) * (2 * size + 1) / 6;
 
-	public static void main(String[] args) {
-		int x, sumofsquares, squareofsum, difference;
-		
-		x=1;
-		sumofsquares = squareofsum = difference =0;
-		for(x=1; x<=100; x++)
-		{
-			sumofsquares += (x*x);
-			squareofsum += x;
-			
-		}
-		System.out.println("The sum of the squares of the first 100 natural numbers: "+sumofsquares);
-		squareofsum = squareofsum * squareofsum;
-		System.out.println("The square of the sum of the first 100 natural numbers is : "+squareofsum);
-		difference = squareofsum - sumofsquares;		
-		
-		System.out.println("Hence the difference between the squares of the sum and the sum of the squares is: "+difference);
+		System.out.println(qos - soq);	
+		} 
 	}
-
-}
-
 //Output: 25164150
