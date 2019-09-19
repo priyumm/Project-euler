c2d849cde28fe10d2805b5fe079be2a68e5585fa
priyumm <53181943+priyumm@users.noreply.github.com>
2019-09-19 21:23:40 +0530

    Update project5.java

--- project5.java
+++ project5.java
@@ -1,26 +1,29 @@
 //Problem 5
 /*What is the smallest positive number 
 that is evenly divisible by all of the numbers from 1 to 20?*/
-import java.math.BigInteger;
+public class project5 {
 
-
-public final class project5{
-	
 	public static void main(String[] args) {
-		System.out.println(new project5().run());
+
+		int i=1,j,c=0,a=0;
+		
+		for(i=21;i<1000000000;i++)
+		{
+			for(j=1;j<=20;j++)
+			{
+				c=i%j;
+				if(c==0)
+					a++;
+			}
+			if(a==20)
+			{
+				System.out.println(i);
+				break;
+             }
+			a=0;
+		}
+           
 	}
-	
-	public String run() {
-		BigInteger allLcm = BigInteger.ONE;
-		for (int i = 1; i <= 20; i++)
-			allLcm = lcm(BigInteger.valueOf(i), allLcm);
-		return allLcm.toString();
-	}
-	
-	
-	private static BigInteger lcm(BigInteger x, BigInteger y) {
-		return x.divide(x.gcd(y)).multiply(y);
-	}
-	
+
 }
 //Output: 232792560
